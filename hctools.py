a359737a03bc9e950aae1b2a5f1e640ccb74cdde
Daniel <czhdaniel@gmail.com>
2022-03-01 23:15:26 +0800

    Changed file structure for OBS

--- hctools.py
+++ hctools.py
@@ -10,7 +10,7 @@ from datetime import datetime, timedelta
 OBSUTIL_PREFIX = './../obsutil/obsutil'
 
 def uploadFoodObject(imagePath, userId):
-    time = (datetime.now()+timedelta(hours=8)).strftime("%Y-%m-%d-%X")
+    time = (datetime.now()+timedelta(hours=8)).strftime("%Y-%m-%d/%X")
     cmd = f'{OBSUTIL_PREFIX} cp {imagePath} obs://spark-food-images/{userId}/{time}.jpeg'
     process = subprocess.run(cmd, shell=True, capture_output=True)
     pprint(process)
