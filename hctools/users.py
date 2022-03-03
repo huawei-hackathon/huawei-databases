18f9b2146d47ed1751ea0039ef616a54b67df1e3
Daniel <czhdaniel@gmail.com>
2022-03-03 18:57:21 +0800

    users

--- hctools/users.py
+++ hctools/users.py
@@ -12,8 +12,13 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-def createElderly(name, age, caregiverName):
-    pass
+def createElderly(name, age, caregiverUserId):
+    sqlCommand = f"INSERT INTO `elderly` (name, age, caregiverUserId) VALUES ('{name}', {age}, {caregiverUserId})"
+    mycursor.execute(sqlCommand)
+    mydb.commit()
+    mycursor.execute("SELECT LAST_INSERT_ID()")
+    userId = mycursor.fetchone()[0]
+    return {'userId': userId}
 
 def createCaregiver(name, password):
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
@@ -21,9 +26,10 @@ def createCaregiver(name, password):
     # Put hashed as string into database
     sqlCommand = f"INSERT INTO `caregivers` (name, password) VALUES ('{name}', '{hashed}')"
     mycursor.execute(sqlCommand)
+    mydb.commit()
     mycursor.execute("SELECT LAST_INSERT_ID()")
     caregiverUserId = mycursor.fetchone()[0]
-    return caregiverUserId
+    return {'caregiverUserId': caregiverUserId}
 
 def authenticateCaregiver(caregiverUserId, password):
     sqlCommand = f"\
@@ -33,15 +39,30 @@ def authenticateCaregiver(caregiverUserId, password):
     hashed = mycursor.fetchone()[2]
     hashed = hashed.encode('utf-8')
     result = bcrypt.checkpw(password.encode('utf-8'), hashed)
-    return result
+    return {'status': result}
 
 def getElderlyProfile(userId):
-    pasr
+    sqlCommand = f"\
+        SELECT * FROM `elderly` WHERE userId = {userId} \
+    "
+    mycursor.execute(sqlCommand)
+    result = mycursor.fetchone()
+    if result == None:
+        return {}
+    else:
+        return {
+            'userId': result[0],
+            'name': result[1],
+            'age': result[2],
+            'caregiverUserId': result[3]
+        }
 
 if __name__ == '__main__':
     id = createCaregiver('daren', 'testing')
     print(authenticateCaregiver(id, 'test'))
     print(authenticateCaregiver(id, 'testing'))
+    userId = createElderly('glenda', 18, 27)
+    print(getElderlyProfile(userId))
     #mycursor.execute("SELECT * FROM `caregivers`")
     #print(mydb)
 
