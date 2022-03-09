54baaa277f9177b6b409a4533f66cba2754a7acc
dvdg6566 <czhdaniel@gmail.com>
2022-03-09 23:59:14 +0800

    Format generate report

--- generateReport.py
+++ generateReport.py
@@ -19,101 +19,106 @@ def generateReport():
     elderlyUserId = users.createElderly('Demonstration Elderly', 78, caregiverUserId, 170, 65, 22.49, 'male')['userId']
     print(f"elderlyUserId: {elderlyUserId}")
 
-''' REPORT '''
-reportUUID = uuid4()
-reports.generateReport(reportUUID, elderlyUserId)
-print("REPORT URL: ", f'http://119.13.104.214:80/getReport/{reportUUID}')
+    ''' REPORT '''
+    reportUUID = uuid4()
+    reports.generateReport(reportUUID, elderlyUserId)
+    print("REPORT URL: ", f'http://119.13.104.214:80/getReport/{reportUUID}')
 
-''' GENERATING HEALTH SIGNALS '''
-now = datetime.now()
-initDate = datetime(now.year, now.month, 1, 0, 0, 0)
-endDate = datetime(now.year, now.month, 1, 0, 0, 0) + relativedelta(months = 1) - timedelta (seconds=1)
-print("GENERATING HEALTH DATA")
+    ''' GENERATING HEALTH SIGNALS '''
+    now = datetime.now()
+    initDate = datetime(now.year, now.month, 1, 0, 0, 0)
+    endDate = datetime(now.year, now.month, 1, 0, 0, 0) + relativedelta(months = 1) - timedelta (seconds=1)
+    print("GENERATING HEALTH DATA")
 
-bar = Bar("Processing...", max=31)
-day = 1
-while initDate < endDate:
-    ''' CREATE SQL UPDATES '''
-    timestr = (initDate+ timedelta(seconds = randrange(0, 7200))).strftime("%Y-%m-%d, %H:%M:%S")
-    healthInfo.updateHealthInformation('heartRate', elderlyUserId, randrange(70, 81), timestr)
-    healthInfo.updateHealthInformation('stepAsymmetry', elderlyUserId, randrange(1,6), timestr)
-    if initDate.hour == 9:
-        healthInfo.updateHealthInformation('sleepSeconds', elderlyUserId, randrange(6*3600, 8*3600), timestr)
-    
-    ''' STEP COUNT '''
-    hour = [0, 3, 6, 9, 12, 15, 18, 21]
-    low = [0, 0, 0, 100, 400, 600, 800, 1000]
-    high = [0, 5, 10, 200, 600, 800, 900, 1250]
-    ind = hour.index(initDate.hour)
-    healthInfo.updateHealthInformation('stepCount', elderlyUserId, randrange(low[ind], high[ind]), timestr)
-    
-    ''' UPDATE DATE AND PROGRESS BAR'''
-    initDate = initDate + timedelta(hours=3)
-    if initDate.day > day:
-        day += 1
-        bar.next()
-print()
-print("HEALTH DATA COMPLETE")
+    bar = Bar("Processing...", max=31)
+    day = 1
+    while initDate < endDate:
+        ''' CREATE SQL UPDATES '''
+        timestr = (initDate+ timedelta(seconds = randrange(0, 7200))).strftime("%Y-%m-%d, %H:%M:%S")
+        healthInfo.updateHealthInformation('heartRate', elderlyUserId, randrange(70, 81), timestr)
+        healthInfo.updateHealthInformation('stepAsymmetry', elderlyUserId, randrange(1,6), timestr)
+        if initDate.hour == 9:
+            healthInfo.updateHealthInformation('sleepSeconds', elderlyUserId, randrange(6*3600, 8*3600), timestr)
+        
+        ''' STEP COUNT '''
+        hour = [0, 3, 6, 9, 12, 15, 18, 21]
+        low = [0, 0, 0, 100, 400, 600, 800, 1000]
+        high = [0, 5, 10, 200, 600, 800, 900, 1250]
+        ind = hour.index(initDate.hour)
+        healthInfo.updateHealthInformation('stepCount', elderlyUserId, randrange(low[ind], high[ind]), timestr)
+        
+        ''' UPDATE DATE AND PROGRESS BAR'''
+        initDate = initDate + timedelta(hours=3)
+        if initDate.day > day:
+            day += 1
+            bar.next()
+    print()
+    print("HEALTH DATA COMPLETE")
 
-''' GENERATE BLUETOOTH FOR ROOMS '''
-initDate = datetime(now.year, now.month, 1, 0, 0, 0)
-endDate = datetime(now.year, now.month, 1, 0, 0, 0) + relativedelta(months = 1) - timedelta (seconds=1)
-ROOM = 'Bedroom'
-roomlist = ['Outside', 'Living Room', 'Bedroom', 'Bathroom', 'Kitchen']
+    ''' GENERATE BLUETOOTH FOR ROOMS '''
+    initDate = datetime(now.year, now.month, 1, 0, 0, 0)
+    endDate = datetime(now.year, now.month, 1, 0, 0, 0) + relativedelta(months = 1) - timedelta (seconds=1)
+    ROOM = 'Bedroom'
+    roomlist = ['Outside', 'Living Room', 'Bedroom', 'Bathroom', 'Kitchen']
 
-print("GENERATING BLUETOOTH ROOM DATA")
-bar = Bar("Processing...", max=31)
-day = 1
-cooldown = False
+    print("GENERATING BLUETOOTH ROOM DATA")
+    bar = Bar("Processing...", max=31)
+    day = 1
+    cooldown = False
 
-while initDate < endDate:
-    if initDate.day > day:
-        day += 1
-        bar.next()
-        cooldown = False
+    while initDate < endDate:
+        if initDate.day > day:
+            day += 1
+            bar.next()
+            cooldown = False
 
-    ''' GO TOILET '''
-    if randrange(1,10) <= 5:
-        timestr = initDate.strftime("%Y-%m-%d, %H:%M:%S")
-        bluetooth.locationUpdate(elderlyUserId, "Bathroom", timestr)
-        initDate += timedelta(minutes = randrange(4, 8))
-        timestr = initDate.strftime("%Y-%m-%d, %H:%M:%S")
-        bluetooth.locationUpdate(elderlyUserId, ROOM, timestr)
-        initDate += timedelta(minutes = normal(60, 10), seconds = 1)
-        continue
-
-    ''' SLEEP ''' 
-    if initDate.hour >= 22 or initDate.hour < 7:
-        if ROOM != 'Bedroom':
+        ''' GO TOILET '''
+        if randrange(1,10) <= 5:
             timestr = initDate.strftime("%Y-%m-%d, %H:%M:%S")
-            bluetooth.locationUpdate(elderlyUserId, "Bedroom", timestr)
-            ROOM = "Bedroom"
-        initDate = initDate + timedelta(minutes = normal(640, 30), seconds=1)
-        continue
-    
-    ''' TIME TO GO OUT '''
-    if initDate.hour >= 11 and initDate.hour <= 19 and not cooldown:
-        if randrange(1,12) <= 4:
+            bluetooth.locationUpdate(elderlyUserId, "Bathroom", timestr)
+            initDate += timedelta(minutes = randrange(4, 8))
             timestr = initDate.strftime("%Y-%m-%d, %H:%M:%S")
-            bluetooth.locationUpdate(elderlyUserId, "Outside", timestr)
-            initDate += timedelta(minutes = normal(180, 50), seconds=1)
-            timestr = initDate.strftime("%Y-%m-%d, %H:%M:%S")
-            bluetooth.locationUpdate(elderlyUserId, "Living Room", timestr)
-            ROOM = "Living Room"
-            cooldown = True
+            bluetooth.locationUpdate(elderlyUserId, ROOM, timestr)
             initDate += timedelta(minutes = normal(60, 10), seconds = 1)
             continue
 
-    ''' RANDOM ROOM CHANGE '''
-    if randrange(1,8) <= 7:
-        rooms = ['Living Room'] * 10 + ['Kitchen'] * 10 + ['Bedroom']
-        ''' REDUCE CHANCE OF GOING TO BEDROOM '''
-        rooms = [i for i in rooms if i != ROOM]
-        target = choice(rooms)
-        timestr = initDate.strftime("%Y-%m-%d, %H:%M:%S")
-        bluetooth.locationUpdate(elderlyUserId, target, timestr)
-        ROOM = target
-        initDate += timedelta(minutes = normal(60, 10), seconds = 1)
+        ''' SLEEP ''' 
+        if initDate.hour >= 22 or initDate.hour < 7:
+            if ROOM != 'Bedroom':
+                timestr = initDate.strftime("%Y-%m-%d, %H:%M:%S")
+                bluetooth.locationUpdate(elderlyUserId, "Bedroom", timestr)
+                ROOM = "Bedroom"
+            initDate = initDate + timedelta(minutes = normal(640, 30), seconds=1)
+            continue
+        
+        ''' TIME TO GO OUT '''
+        if initDate.hour >= 11 and initDate.hour <= 19 and not cooldown:
+            if randrange(1,12) <= 4:
+                timestr = initDate.strftime("%Y-%m-%d, %H:%M:%S")
+                bluetooth.locationUpdate(elderlyUserId, "Outside", timestr)
+                initDate += timedelta(minutes = normal(180, 50), seconds=1)
+                timestr = initDate.strftime("%Y-%m-%d, %H:%M:%S")
+                bluetooth.locationUpdate(elderlyUserId, "Living Room", timestr)
+                ROOM = "Living Room"
+                cooldown = True
+                initDate += timedelta(minutes = normal(60, 10), seconds = 1)
+                continue
 
-print()
-print("BLUETOOTH ROOM DATA COMPLETE")
+        ''' RANDOM ROOM CHANGE '''
+        if randrange(1,8) <= 7:
+            rooms = ['Living Room'] * 10 + ['Kitchen'] * 10 + ['Bedroom']
+            ''' REDUCE CHANCE OF GOING TO BEDROOM '''
+            rooms = [i for i in rooms if i != ROOM]
+            target = choice(rooms)
+            timestr = initDate.strftime("%Y-%m-%d, %H:%M:%S")
+            bluetooth.locationUpdate(elderlyUserId, target, timestr)
+            ROOM = target
+            initDate += timedelta(minutes = normal(60, 10), seconds = 1)
+
+    print()
+    print("BLUETOOTH ROOM DATA COMPLETE")
+    return {
+        'caregiverUserId': caregiverUserId
+        'elderlyUserId': elderlyUserId
+        'url': f'http://119.13.104.214:80/getReport/{reportUUID}'
+    }
